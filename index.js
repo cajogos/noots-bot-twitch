b988501fa27688b03925765392c75d29bc93047c
Carlos Ferreira <c@rlos.rocks>
2022-10-04 23:36:01 +0100

    Useful tools for the Noots Bot

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { RefreshingAuthProvider } from '@twurple/auth';
 import { ChatClient } from '@twurple/chat';
-import { clientId, clientSecret } from './credentials.js';
-
+import { ApiClient, ChattersList } from '@twurple/api';
 import { promises as fs } from 'fs';
+
+import { clientId, clientSecret } from './credentials.js';
 
 const tokensFile = './tokens.json';
 const fileEncoding = 'UTF-8';
@@ -11,7 +12,8 @@ const trackedChannels = [
     'kingmcewan',
     'cajogos',
     'yeekaycrafts',
-    'lifeofbeard'
+    'lifeofbeard',
+    'mrricardo94'
 ];
 
 async function main()
@@ -33,15 +35,48 @@ async function main()
         channels: trackedChannels
     });
 
-    chatClient.onMessage((channel, user, text) =>
+    const apiClient = new ApiClient({ authProvider });
+
+
+    chatClient.onMessage(async (channel, username, text) =>
     {
-        console.log(`[${channel}] ${user}: ${text}`);
+        console.log(`[${channel}] ${username}: ${text}`);
 
         if (text === 'nootnoot')
         {
             chatClient.say(channel, 'NOOT NOOT!');
+
+            const user = await apiClient.users.getUserByName(username);
+
+            console.log('*** FOLLOWS ***');
+            let follows = await apiClient.users.getFollows({ user: user.id });
+            console.log(`${username} follows ${follows.total} channels`);
+            for (const follow of follows.data)
+            {
+                console.log(follow.followedUserId, follow.followedUserDisplayName, follow.followedUserName, follow.followDate.toLocaleDateString());
+            }
+
+            console.log('*** FOLLOWERS ***');
+            let followers = await apiClient.users.getFollows({ followedUser: user.id });
+            console.log(`${username} has ${followers.total} followers`);
+            for (const follower of followers.data)
+            {
+                console.log(follower.userId, follower.userDisplayName, follower.userName);
+            }
+
+            let channelUser = await apiClient.users.getUserByName(channel.replace('#', ''));
+            let channelObject = await apiClient.channels.getChannelInfoById(channelUser.id);
+            console.log(`${channelObject.displayName} is playing ${channelObject.gameName}.`);
+
+            let chatters = await apiClient.unsupported.getChatters(channelObject.name);
+            for (const chatter of chatters.allChattersWithStatus)
+            {
+                console.log(chatter);
+            }
         }
     });
+
+
 
     await chatClient.connect();
 }

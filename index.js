47154760571f376fe4a4249dcabb53914a34a3eb
Carlos Ferreira <c@rlos.rocks>
2022-10-04 22:02:02 +0100

    Tracked channels list

--- index.js
+++ index.js
@@ -6,26 +6,31 @@ import { promises as fs } from 'fs';
 
 const tokensFile = './tokens.json';
 const fileEncoding = 'UTF-8';
+const trackedChannels = [
+    'pudgyycat',
+    'kingmcewan',
+    'cajogos',
+    'yeekaycrafts',
+    'lifeofbeard'
+];
 
 async function main()
 {
     const tokenData = JSON.parse(await fs.readFile(tokensFile, fileEncoding));
 
     const authProvider = new RefreshingAuthProvider({
-        clientId, clientSecret,
+        clientId,
+        clientSecret,
         onRefresh: async newTokenData => await fs.writeFile(
             tokensFile,
             JSON.stringify(newTokenData, null, 4),
             fileEncoding
-        ),
+        )
     }, tokenData);
 
     const chatClient = new ChatClient({
         authProvider,
-        channels: [
-            'pudgyycat',
-            'kingmcewan'
-        ]
+        channels: trackedChannels
     });
 
     chatClient.onMessage((channel, user, text) =>
